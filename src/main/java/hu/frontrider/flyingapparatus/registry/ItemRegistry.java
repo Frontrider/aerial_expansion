14342f624de6562708171a95d937b3887ef98a13
András Kis <kisandrasgabor@gmail.com>
2018-05-04 23:31:25 +0200

    finished recipes.

--- src/main/java/hu/frontrider/flyingapparatus/registry/ItemRegistry.java
+++ src/main/java/hu/frontrider/flyingapparatus/registry/ItemRegistry.java
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class ItemRegistry {
 
 
-    public static final FlyingApparatusItem mmu_normal = new FlyingApparatusItem(2000, "apparatus_basic");
+    public static final FlyingApparatusItem mmu_basic = new FlyingApparatusItem(2000, "apparatus_basic");
     public static final FlyingApparatusItem mmu_hardened = new FlyingApparatusItem(5000, "apparatus_hardened", true, 400);
     public static final FlyingApparatusItem mmu_reinforced = new FlyingApparatusItem(8000, "apparatus_reinforced", true, 200, .3);
     public static final FlyingApparatusItem mmu_signalum = new FlyingApparatusItem(12000, "apparatus_signalum", true, 100, .5);
@@ -34,7 +34,7 @@ public class ItemRegistry {
     @SubscribeEvent
     public static void init(RegistryEvent.Register<Item> event) {
         event.getRegistry().registerAll(
-                mmu_normal, mmu_hardened, mmu_reinforced, mmu_signalum, mmu_resonant,
+                mmu_basic, mmu_hardened, mmu_reinforced, mmu_signalum, mmu_resonant,
                // normal_rocket_pants,
                 handheld,
                 flux_tube, armor_frame,
